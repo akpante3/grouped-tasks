59e51eba4c8c47af0d979fabd65992610f7a0879
Victor <career.victorobije@gmail.com>
2024-02-27 21:04:49 +0100

    drop-down-component

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/DropDown.scss'
+
+function DropDown() {
+  return (
+    <div className='drop-down'>
+       <div>
+        Group
+       </div>
+       <div>Show</div>
+    </div>
+  );
+}
+
+export default DropDown;
